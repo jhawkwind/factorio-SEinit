be18e28faa6f5390496d58e5ac196db86eea103e
Kelvin Mok <kai@outlawstar.net>
2018-12-24 21:06:44 -0800

    Update permissions
    Forgot the other objects.
    
    Forgot the role transition
    
    Syntax error.
    
    More changes!
    
    And NOT those contexts, start the context.
    
    Corrected regex matching
    
    Missed a negative.
    
    Additional perms for the init_t
    
    Adjust the FC contexts for denials
    
    Make things blocking now.
    
    Absolute path check
    
    Need access to runcon from the init_t
    
    factorio_init.te:70: Warning: corecmd_sbin_entry_type() has been deprecated, please use corecmd_bin_entry_type() instead.
    
    So, OK!
    
    Oops, copy pasta error.
    
    Change the way how the server is initialized.
    
    Change the check for a server settings file.
    
    Wrong function call
    
    Put in the other bypass.
    
    Check for file existence not entry existence.

--- selinux/compile.sh
+++ selinux/compile.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+
+# Make sure the script is CD'ed to a PWD that is valid and not symlinked. (ie. where the script actually is at)
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "${SOURCE}" ]; do # resolve ${SOURCE} until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
+  SOURCE="$(readlink "${SOURCE}")"
+  [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}" # if ${SOURCE} was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd "${SCRIPT_DIR}"
+
 set -x
 /bin/bash ./factorio/factorio.sh ${1}
+
+cp ./factorio_init/factorio_init.if ./factorio_init/factorio_init.if.backup
+cat ./factorio_init/factorio.if >> ./factorio_init/factorio_init.if
 /bin/bash ./factorio_init/factorio_init.sh ${1}
+cat ./factorio_init/factorio_init.if.backup > ./factorio_init/factorio_init.if
 
 pwd=$(pwd)
 #rpmbuild --define "_sourcedir ${pwd}/build" --define "_specdir ${pwd}/build" --define "_builddir ${pwd}/build" --define "_srcrpmdir ${pwd}/build" --define "_rpmdir ${pwd}/build" --define "_buildrootdir ${pwd}/build/.build"  -ba ${pwd}/build/factorio_selinux.spec
