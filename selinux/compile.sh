90940f0c6bf50a6209f94194585de81f334ace5e
Kelvin Mok <kai@outlawstar.net>
2018-12-26 00:01:23 -0800

    Add missing permissions.

--- selinux/compile.sh
+++ selinux/compile.sh
@@ -12,11 +12,17 @@ cd "${SCRIPT_DIR}"
 
 set -x
 /bin/bash ./factorio/factorio.sh ${1}
+
 sleep 3; # Need to wait for the disk process to catch up on slower systems to complete installation of the files to avoid contention/deadlock.
+
 cp ./factorio_init/factorio_init.if ./factorio_init/factorio_init.if.backup
 cat ./factorio_init/factorio.if >> ./factorio_init/factorio_init.if
 /bin/bash ./factorio_init/factorio_init.sh ${1}
 cat ./factorio_init/factorio_init.if.backup > ./factorio_init/factorio_init.if
+
+sleep 3;
+
+semodule --disable_dontaudit --build # For Debugging purposes.
 
 pwd=$(pwd)
 #rpmbuild --define "_sourcedir ${pwd}/build" --define "_specdir ${pwd}/build" --define "_builddir ${pwd}/build" --define "_srcrpmdir ${pwd}/build" --define "_rpmdir ${pwd}/build" --define "_buildrootdir ${pwd}/build/.build"  -ba ${pwd}/build/factorio_selinux.spec
